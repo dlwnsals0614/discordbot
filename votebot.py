50f9578595c572e2f8cfec7dd694d55d3f2878f9
dlwnsals0614 <84721375+dlwnsals0614@users.noreply.github.com>
2021-08-28 11:04:14 +0900

    Update votebot.py

--- votebot.py
+++ votebot.py
@@ -15,10 +15,10 @@ async def on_ready(): # 봇이 실행되면 터미널에서 출력된다
 async def on_message(message):
     if message.content.startswith("투표"):
         vote = message.content[4:].split("/")
-        await message.channel.send("투표 " + vote[0])
-        for i in range(1, len(vote)):
-            choose = await message.channel.send("```" + vote[i] + "```")
-            await choose.add_reaction('👍')
+            await message.channel.send("투표 - " + vote[0])
+            for i in range(1, len(vote)):
+                choose = await message.channel.send("```" + vote[i] + "```")
+                await choose.add_reaction('👍')
             
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
